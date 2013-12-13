58f0b4ae14c4470acbfd169922a533e37d7efce8
luca992 <spinazzo@usc.edu>
2013-12-13 03:59:57 -0800

    Tide chart images are now set for each buoy

--- Buoy Buddy/LMSTidesViewController.m
+++ Buoy Buddy/LMSTidesViewController.m
@@ -13,6 +13,7 @@
 
 @property (strong, nonatomic) LMSBuoyBuddyModel *buoyBuddyModel;
 @property (nonatomic, weak) NSString *buoyName;
+@property (weak, nonatomic) IBOutlet UIImageView *tideImageView;
 
 @end
 @implementation LMSTidesViewController
@@ -20,6 +21,24 @@
 {
     [super viewDidLoad];
     _buoyBuddyModel = [LMSBuoyBuddyModel sharedModel];
+    if ([_buoyName isEqual: @"Santa Monica Bay Buoy"]){
+    UIImage *image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://tile.surfline.com/dashboard/dashboards/4/9//tidebar_4900_0_ft_en.png"]]];
+    [_tideImageView setImage:image];
+    }
+    if ([_buoyName isEqual: @"Santa Monica Basin Buoy"]){
+        UIImage *image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://tile.surfline.com/dashboard/dashboards/4/9//tidebar_4900_0_ft_en.png"]]];
+        [_tideImageView setImage:image];
+    }
+    if ([_buoyName isEqual: @"Block Island Buoy"]){
+        UIImage *image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://tile.surfline.com/dashboard/dashboards/5/1//tidebar_5107_0_ft_en.png"]]];
+        [_tideImageView setImage:image];
+    }
+    if ([_buoyName isEqual: @"Montauk Point Buoy"]){
+        UIImage *image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://tile.surfline.com/dashboard/dashboards/4/2//tidebar_4268_0_ft_en.png"]]];
+        [_tideImageView setImage:image];
+    }
+
+    
     
 }
 
